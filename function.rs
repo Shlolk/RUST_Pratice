f0db02553335bd8215102aa0401a0a11e9dc373c
Shlok kumar <shlok200417@gmail.com>
2026-08-27 02:28:06 +0530

    Add a simple Rust function to print a message

---
+++ function.rs
@@ -0,0 +1,10 @@
+// now we gonna learn how to function in the rust i mean how to delecared rust 
+fn main(){
+  define_x();//it is the function call 
+}
+
+//here we are delecaring the function 
+fn define_x (){
+  let x = "hello";
+  println!("{}",x);
+}
