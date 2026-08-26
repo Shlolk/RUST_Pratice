65f9fcf0530d7fab131da3d2e85c86ba6dc68887
Shlok kumar <shlok200417@gmail.com>
2026-08-27 01:55:15 +0530

    Fix unused variable warning in first.rs
    Use underscore for unused variable to avoid warnings.

--- first.rs
+++ first.rs
@@ -4,3 +4,12 @@ fn main () {
     assert_eq!(x,5);
     println!("success");
 }
+//in the first its show the out but due the exrat assign the varible like y where we are not assign it showing a sweet waring on that so 
+// we gonna fix that by using the underscore it meams to use because it help to understand compiler that igonre that thing
+//here can use in the below code how to use it 
+
+fn main (){
+    let x :i31 = 10;
+    let _y:i32;// here complier ignore this delecared variable .....
+    assert_eq!(x,10);
+    println!("succes");
