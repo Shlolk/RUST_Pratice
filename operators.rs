b09c00734134397483ea550ae2dddd97c058548c
Shlok kumar <shlok200417@gmail.com>
2026-08-30 19:17:55 +0530

    Add basic arithmetic operations in operators.rs

---
+++ operators.rs
@@ -0,0 +1,13 @@
+fn main(){
+//arthematic operators 
+let a = 23;
+let b = 24 ;
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+let div = a/b;
+let rem = a%b;
+println!("{},{},{},{},{}",sum,sub,mul,div,rem);
+
+
+}
