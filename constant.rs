275084e7bf80a28085065959e651e885bd92823b
Shlok kumar <shlok200417@gmail.com>
2026-08-30 19:01:46 +0530

    Add constant example in constant.rs

---
+++ constant.rs
@@ -0,0 +1,6 @@
+// in this we write the constant code where using constant 
+//so here we are using const keyword and with declaring what datatype will store in this 
+fn main(){
+const NAME:&str="shlok kumar";
+println!("{}",NAME);
+}
